c97fb734d2c98ae6b6e9e134c288798d02899456
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-17 14:37:40 -0300

    fix: Ajustes na Hero

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -26,9 +26,9 @@ export default function Index() {
       <Hero
         colorMode={colorMode}
         appDownloadButtons={false}
-        defaultHero={true}
+        defaultHero={false}
         influencer={false}
-        mesclado={false}
+        mesclado={true}
       />
       <LaborAction />
 
