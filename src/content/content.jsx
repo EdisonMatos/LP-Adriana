ffbc892d41f2f5f2668af54e6f3f7af063549b6c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-25 15:23:56 -0300

    fix: Ajustes de copy

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -457,13 +457,9 @@ const content = {
     laborAction: {
       miniTag: "ATUAÇÃO TRABALHISTA",
       title:
-        "Atuação na defesa do trabalhador, buscando garantir a reparação de injustiças e o reconhecimento dos direitos previstos em lei.",
+        " A Dra. Adriana Sereno atua na defesa do trabalhador, buscando garantir a reparação de injustiças e o reconhecimento dos direitos previstos em lei.",
       text: (
         <p>
-          A Dra. Adriana Sereno atua na defesa do trabalhador, buscando garantir
-          a reparação de injustiças e o reconhecimento dos direitos previstos em
-          lei.
-          <br />
           <br /> DIREITO DO TRABALHO
           <br />⚖ Principais causas na justiça do trabalho:
           <br />
