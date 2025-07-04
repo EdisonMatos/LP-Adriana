506ed65b19d4be65c6536e877ce8264f8075a66f
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-04 09:06:55 -0300

    fix: Cta copy

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -491,7 +491,7 @@ const content = {
       backgroundImg: ctaWhatsappImgBg,
       miniTag: "FALE CONOSCO",
       title:
-        "Enfrentando um problema jurídico? Deixe que a Dra. Adriana Sereno cuida disso para você!",
+        "Enfrentando um problema jurídico? Deixe que cuidamos disso para você!",
       subtitle:
         "Converse com nossa equipe. Estamos prontos para oferecer a orientação jurídica de que você precisa.",
       ctaButtonText: "Quero falar com a advogada",
